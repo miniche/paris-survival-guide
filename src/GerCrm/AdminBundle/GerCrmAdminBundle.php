ac843ca2ebce0933aa57a144600598d8c1bf8d24
Charles-Emmanuel <che.camus@gmail.com>
2014-12-04 11:13:55 +0100

    Initialized Symfony 2.5 from a project

---
+++ src/GerCrm/AdminBundle/GerCrmAdminBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GerCrm\AdminBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GerCrmAdminBundle extends Bundle
+{
+}
---
+++ src/GerCrm/UserBundle/GerCrmUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GerCrm\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GerCrmUserBundle extends Bundle
+{
+}
