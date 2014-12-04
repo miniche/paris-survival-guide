5b266a5bef8176857ba6f2b142e4decbf35ffa38
Charles-Emmanuel <che.camus@gmail.com>
2014-12-04 18:33:56 +0100

    Added Geo localization, improved entities and commands

---
+++ src/ParisSurvivalGuide/AppBundle/Command/ImportAddressesDataCommand.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ParisSurvivalGuide\AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class ImportAddressesDataCommand extends ContainerAwareCommand
+{
+    /**
+     * @var InputInterface
+     */
+    protected $input;
+
+    /**
+     * @var OutputInterface
+     */
+    protected $output;
+
+    /**
+     * @inheritdoc
+     * @see \Symfony\Component\Console\Command\Command::configure()
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('psg:process-address-data')
+            ->setDescription('Load meteo data from the CSV file')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        echo "Starting...";
+        $this->getContainer()->get('paris_survival_guide_app.accidents_address')->import();
+    }
+}
